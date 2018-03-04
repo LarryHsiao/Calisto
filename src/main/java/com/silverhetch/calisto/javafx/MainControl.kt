3bd434103878e05b0075ed3c0aa8bd1e1a29763c
Larry Hsiao <larryhsiao@silverhetch.com>
2018-03-04 19:39:21 +0800

    ShowTagManagement after click menu

--- src/main/java/com/silverhetch/calisto/javafx/MainControl.kt
+++ src/main/java/com/silverhetch/calisto/javafx/MainControl.kt
@@ -1,15 +1,29 @@
 package com.silverhetch.calisto.javafx
 
 import com.silverhetch.calisto.javafx.`object`.DragToInsert
+import javafx.event.ActionEvent
 import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
 import javafx.fxml.Initializable
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.stage.Stage
 import java.net.URL
 import java.util.*
 
 class MainControl : Initializable {
     @FXML
-    var dragToInsertController: DragToInsert? = null
+    private var dragToInsertController: DragToInsert? = null
+    private var resources: ResourceBundle? = null;
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
+        this.resources = resources;
+    }
+
+    fun tagManagement(actionEvent: ActionEvent) {
+        val root = FXMLLoader.load<Parent>(javaClass.getResource("/fxml/TagManagement.fxml"), resources)
+        val dialog = Stage()
+        dialog.scene = Scene(root)
+        dialog.showAndWait()
     }
 }
