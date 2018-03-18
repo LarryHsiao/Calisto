cb1a487523016eadfa2aae306bb5c544353c20c0
Larry Hsiao <larryhsiao@silverhetch.com>
2018-03-19 06:18:43 +0800

    config file with multiple search location

--- src/main/java/com/silverhetch/calisto/config/ConfigurationFactory.java
+++ src/main/java/com/silverhetch/calisto/config/ConfigurationFactory.java
@@ -1,7 +1,33 @@
 package com.silverhetch.calisto.config;
 
+import com.silverhetch.calisto.config.file.JsonFile;
+
+import java.io.File;
+
 public class ConfigurationFactory {
+    private final static File DEFAULT_CONFIG;
+    private final static File[] LOADING_CANDIDATE;
+
+    static {
+        DEFAULT_CONFIG = new File(ClassLoader.getSystemClassLoader().getResource(".").getPath());
+        LOADING_CANDIDATE = new File[]{
+                new File(System.getProperty("user.home") + File.separator + ".calisto.config"),
+        };
+    }
+
     public Configuration config() {
-        return new JsonConfiguration(new DefaultConfiguration());
+        return new JsonConfiguration(
+                new JsonFile(validConfigJsonPath()),
+                new DefaultConfiguration()
+        );
+    }
+
+    private String validConfigJsonPath() {
+        for (File file : LOADING_CANDIDATE) {
+            if (file.exists()) {
+                return file.getAbsolutePath();
+            }
+        }
+        return DEFAULT_CONFIG.getAbsolutePath();
     }
 }
--- src/main/java/com/silverhetch/calisto/config/JsonConfiguration.kt
+++ src/main/java/com/silverhetch/calisto/config/JsonConfiguration.kt
@@ -1,10 +1,10 @@
 package com.silverhetch.calisto.config
 
+import com.silverhetch.calisto.config.file.Json
 import com.silverhetch.calisto.config.file.JsonFile
 import java.io.File
 
-internal class JsonConfiguration(private val default: Configuration) : Configuration {
-    private val jsonFile: JsonFile = JsonFile(System.getProperty("user.home") + File.separator + ".calisto.config")
+internal class JsonConfiguration(private val jsonFile:Json,private val default: Configuration) : Configuration {
     override fun changeHome(path: String?): File {
         jsonFile.rewrite("workspaceFile", path!!)
         return File(path)
