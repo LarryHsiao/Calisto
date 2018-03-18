fd7c1a7d67f57cb4913c69e771167825b6fd8f56
Larry Hsiao <larryhsiao@silverhetch.com>
2018-03-19 06:42:26 +0800

    jason config relate class refactor

--- src/main/java/com/silverhetch/calisto/config/ConfigurationFactory.java
+++ src/main/java/com/silverhetch/calisto/config/ConfigurationFactory.java
@@ -5,29 +5,10 @@ import com.silverhetch.calisto.config.file.JsonFile;
 import java.io.File;
 
 public class ConfigurationFactory {
-    private final static File DEFAULT_CONFIG;
-    private final static File[] LOADING_CANDIDATE;
-
-    static {
-        DEFAULT_CONFIG = new File(ClassLoader.getSystemClassLoader().getResource(".").getPath());
-        LOADING_CANDIDATE = new File[]{
-                new File(System.getProperty("user.home") + File.separator + ".calisto.config"),
-        };
-    }
-
     public Configuration config() {
         return new JsonConfiguration(
-                new JsonFile(validConfigJsonPath()),
+                new JsonFile(new File(System.getProperty("user.home") + File.separator + ".calisto.config")),
                 new DefaultConfiguration()
         );
-    }
-
-    private String validConfigJsonPath() {
-        for (File file : LOADING_CANDIDATE) {
-            if (file.exists()) {
-                return file.getAbsolutePath();
-            }
-        }
-        return DEFAULT_CONFIG.getAbsolutePath();
     }
 }
--- src/main/java/com/silverhetch/calisto/config/file/JsonFile.kt
+++ src/main/java/com/silverhetch/calisto/config/file/JsonFile.kt
@@ -3,17 +3,18 @@ package com.silverhetch.calisto.config.file
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import com.google.gson.stream.JsonReader
+import java.io.File
 import java.io.FileReader
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.nio.file.StandardOpenOption.*
 
-internal class JsonFile(private val jsonPath: String) : Json {
+internal class JsonFile(private val jsonFile:File) : Json {
     private var json: JsonObject? = null
     override fun json(): JsonObject {
         return try {
             if (json == null) {
-                json = JsonParser().parse(JsonReader(FileReader(jsonPath))).asJsonObject
+                json = JsonParser().parse(JsonReader(FileReader(jsonFile))).asJsonObject
             }
             json!!
         } catch (e: Exception) {
@@ -22,7 +23,7 @@ internal class JsonFile(private val jsonPath: String) : Json {
     }
 
     override fun rewrite(key: String, value: String) {
-        val path = Paths.get(jsonPath)
+        val path = jsonFile.toPath()
         val json = json()
         json.addProperty(key, value)
         Files.write(path, json.toString().toByteArray(), CREATE, WRITE, TRUNCATE_EXISTING)
