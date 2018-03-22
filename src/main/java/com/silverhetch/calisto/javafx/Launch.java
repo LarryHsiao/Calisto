1c6baaec6014652104931925bbd1ee596193b178
Larry Hsiao <larryhsiao@silverhetch.com>
2018-03-22 23:32:15 +0800

    Using javaFx default style, with our css for some twist.

--- src/main/java/com/silverhetch/calisto/javafx/Launch.java
+++ src/main/java/com/silverhetch/calisto/javafx/Launch.java
@@ -1,5 +1,6 @@
 package com.silverhetch.calisto.javafx;
 
+import com.sun.javafx.css.StyleManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,6 +16,9 @@ public class Launch extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Application.setUserAgentStylesheet(Application.STYLESHEET_MODENA);
+        StyleManager.getInstance().addUserAgentStylesheet(getClass().getResource("/css/General.css").toURI().toString());
+
         Parent parent = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"), ResourceBundle.getBundle("i18n/bundle"));
         Scene scene = new Scene(parent);
         primaryStage.setScene(scene);
